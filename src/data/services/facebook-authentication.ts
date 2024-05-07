68a3dc5919c721851ac2fcee5d72ddf9d6b4d89f
Raymw1 <rayan.rmw@gmail.com>
2024-05-06 21:06:43 -0300

    feat: ensure FacebookAuthenticationService calls TokenGenerator with correct expiration

--- src/data/services/facebook-authentication.ts
+++ src/data/services/facebook-authentication.ts
@@ -3,7 +3,7 @@ import { TokenGenerator } from '@/data/contracts/crypto'
 import { LoadUserAccountRepository, SaveFacebookAccountRepository } from '@/data/contracts/repos'
 import { AuthenticationError } from '@/domain/errors'
 import { FacebookAuthentication } from '@/domain/features'
-import { FacebookAccount } from '@/domain/models'
+import { AccessToken, FacebookAccount } from '@/domain/models'
 
 export class FacebookAuthenticationService implements FacebookAuthentication {
   constructor (
@@ -18,7 +18,7 @@ export class FacebookAuthenticationService implements FacebookAuthentication {
       const accountData = await this.userAccountRepository.load({ email: fbData.email })
       const fbAccount = new FacebookAccount(fbData, accountData)
       const { id } = await this.userAccountRepository.saveWithFacebook(fbAccount)
-      await this.crypto.generateToken({ key: id })
+      await this.crypto.generateToken({ key: id, expirationInMs: AccessToken.expirationInMs })
     }
     return new AuthenticationError()
   }
