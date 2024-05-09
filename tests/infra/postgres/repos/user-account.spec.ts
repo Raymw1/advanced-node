f75fe3e4c4fac5274c91be9f496a44b22adc4377
Raymw1 <rayan.rmw@gmail.com>
2024-05-09 20:31:55 -0300

    test: ensure PgUserAccountRepository returns undefined if email does not exist

--- tests/infra/postgres/repos/user-account.spec.ts
+++ tests/infra/postgres/repos/user-account.spec.ts
@@ -19,6 +19,22 @@ describe('PgUserAccountRepository', () => {
       const accountData = await sut.load({ email: 'existing_email' })
 
       expect(accountData).toEqual({ id: '1' })
+      await connection.close()
+    })
+
+    it('should return undefined if email does not exist', async () => {
+      const db = newDb()
+      const connection = await db.adapters.createTypeormConnection({
+        type: 'postgres',
+        entities: [PgUser]
+      })
+      await connection.synchronize()
+      const sut = new PgUserAccountRepository()
+
+      const accountData = await sut.load({ email: 'any_email' })
+
+      expect(accountData).toBe(undefined)
+      await connection.close()
     })
   })
 })
