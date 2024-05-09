a0fdeb4e2d558ed897c6ea90530bbf41eec434d2
Raymw1 <rayan.rmw@gmail.com>
2024-05-09 20:28:34 -0300

    test: ensure PgUserAccountRepository loads account if emais exists

---
+++ tests/infra/postgres/repos/user-account.spec.ts
@@ -0,0 +1,24 @@
+import { PgUser, PgUserAccountRepository } from '@/infra/postgres/repos'
+
+import { newDb } from 'pg-mem'
+import { getRepository } from 'typeorm'
+
+describe('PgUserAccountRepository', () => {
+  describe('load', () => {
+    it('should return an account if email exists', async () => {
+      const db = newDb()
+      const connection = await db.adapters.createTypeormConnection({
+        type: 'postgres',
+        entities: [PgUser]
+      })
+      await connection.synchronize()
+      const pgUserRepo = getRepository(PgUser)
+      await pgUserRepo.save({ email: 'existing_email' })
+      const sut = new PgUserAccountRepository()
+
+      const accountData = await sut.load({ email: 'existing_email' })
+
+      expect(accountData).toEqual({ id: '1' })
+    })
+  })
+})
