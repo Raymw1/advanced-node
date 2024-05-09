f64ef314958fd9fe4a088997dbefa6384dc7bbdc
Raymw1 <rayan.rmw@gmail.com>
2024-05-09 20:28:04 -0300

    feat: ensure PgUserAccountRepository loads account if emais exists

---
+++ src/infra/postgres/repos/user-account.ts
@@ -0,0 +1,31 @@
+import { LoadUserAccountRepository } from '@/data/contracts/repos'
+
+import { Column, Entity, PrimaryGeneratedColumn, getRepository } from 'typeorm'
+
+@Entity({ name: 'usuarios' })
+export class PgUser {
+  @PrimaryGeneratedColumn()
+  id!: number
+
+  @Column({ name: 'nome', nullable: true })
+  name?: string
+
+  @Column()
+  email!: string
+
+  @Column({ name: 'id_facebook', nullable: true })
+  facebookId?: string
+}
+
+export class PgUserAccountRepository implements LoadUserAccountRepository {
+  async load (params: LoadUserAccountRepository.Params): Promise<LoadUserAccountRepository.Result> {
+    const pgUserRepo = getRepository(PgUser)
+    const pgUser = await pgUserRepo.findOne({ email: params.email })
+    if (pgUser !== undefined) {
+      return {
+        id: pgUser?.id.toString(),
+        name: pgUser?.name ?? undefined
+      }
+    }
+  }
+}
