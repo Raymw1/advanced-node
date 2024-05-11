f125195aae00f88c738f5fe5ff74550b9e77e308
Raymw1 <rayan.rmw@gmail.com>
2024-05-11 20:58:12 -0300

    feat: ensure RequiredStringValidator returns undefined if value is not empty

--- src/application/validation/required-string.ts
+++ src/application/validation/required-string.ts
@@ -4,6 +4,8 @@ export class RequiredStringValidator {
   constructor (private readonly value: string, private readonly fieldName: string) {}
 
   validate (): Error | undefined {
-    return new RequiredFieldError(this.fieldName)
+    if (this.value === '' || this.value === null || this.value === undefined) {
+      return new RequiredFieldError(this.fieldName)
+    }
   }
 }
