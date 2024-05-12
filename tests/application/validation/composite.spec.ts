5fe9bb405b2d23a1281654d32e996cfd7dd785d4
Raymw1 <rayan.rmw@gmail.com>
2024-05-12 10:01:49 -0300

    refactor: isolate ValidationComposite sut creation

--- tests/application/validation/composite.spec.ts
+++ tests/application/validation/composite.spec.ts
@@ -1,15 +1,25 @@
 import { ValidationComposite, Validator } from '@/application/validation'
-import { mock } from 'jest-mock-extended'
+import { MockProxy, mock } from 'jest-mock-extended'
 
 describe('ValidationComposite', () => {
-  it('should return undefined if all Validators return undefined', () => {
-    const validator1 = mock<Validator>()
-    validator1.validate.mockReturnValue(undefined)
-    const validator2 = mock<Validator>()
-    validator2.validate.mockReturnValue(undefined)
-    const validators = [validator1, validator2]
-    const sut = new ValidationComposite(validators)
+  let validator1: MockProxy<Validator>
+  let validator2: MockProxy<Validator>
+  let validators: Validator[]
+  let sut: ValidationComposite
 
+  beforeAll(() => {
+    validator1 = mock<Validator>()
+    validator1.validate.mockReturnValue(undefined)
+    validator2 = mock<Validator>()
+    validator2.validate.mockReturnValue(undefined)
+    validators = [validator1, validator2]
+  })
+
+  beforeEach(() => {
+    sut = new ValidationComposite(validators)
+  })
+
+  it('should return undefined if all Validators return undefined', () => {
     const error = sut.validate()
 
     expect(error).toBeUndefined()
