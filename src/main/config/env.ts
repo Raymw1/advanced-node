d121e23de240a72b54a5ea9000076b311237a074
Raymw1 <rayan.rmw@gmail.com>
2024-05-14 20:49:46 -0300

    feat: configure express server

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,5 +2,6 @@ export const env = {
   facebookApi: {
     clientId: process.env.FB_CLIENT_ID ?? '364210949987071',
     clientSecret: process.env.FB_CLIENT_SECRET ?? '7be8aa7d4bc58a1ece9a421906f3ab65'
-  }
+  },
+  port: process.env.PORT ?? 3333
 }
