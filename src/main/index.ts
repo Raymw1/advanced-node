5195a53bb1c84ba83b12ecbad23e9a2d37341d73
Raymw1 <rayan.rmw@gmail.com>
2024-05-16 22:36:39 -0300

    refactor: move typeorm config to root folder

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,13 +1,12 @@
 import './config/module-alias'
 
-import { config } from '@/infra/postgres/helpers'
 import { app } from '@/main/config/app'
 import { env } from '@/main/config/env'
 
 import 'reflect-metadata'
 import { createConnection } from 'typeorm'
 
-createConnection(config)
+createConnection()
   .then(() => {
     app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
   })
