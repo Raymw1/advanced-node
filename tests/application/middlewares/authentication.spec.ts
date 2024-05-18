5a0cf16a9490e5518b3a8312568a3529f013b5d9
Raymw1 <rayan.rmw@gmail.com>
2024-05-18 16:32:19 -0300

    test: ensure AuthenticationMiddleware returns 403 if authorization is empty

---
+++ tests/application/middlewares/authentication.spec.ts
@@ -0,0 +1,15 @@
+import { ForbiddenError } from '@/application/errors'
+import { AuthenticationMiddleware } from '@/application/middlewares'
+
+describe('AuthenticationMiddleware', () => {
+  it('should return 403 if authorization is empty', async () => {
+    const sut = new AuthenticationMiddleware()
+
+    const httpResponse = await sut.handle({ authorization: '' })
+
+    expect(httpResponse).toEqual({
+      statusCode: 403,
+      data: new ForbiddenError()
+    })
+  })
+})
