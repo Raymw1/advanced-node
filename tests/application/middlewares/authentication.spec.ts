b3a3975474d0d2dbf226cd4b2435372566313f93
Raymw1 <rayan.rmw@gmail.com>
2024-05-18 16:33:01 -0300

    test: ensure AuthenticationMiddleware returns 403 if authorization is null

--- tests/application/middlewares/authentication.spec.ts
+++ tests/application/middlewares/authentication.spec.ts
@@ -12,4 +12,15 @@ describe('AuthenticationMiddleware', () => {
       data: new ForbiddenError()
     })
   })
+
+  it('should return 403 if authorization is null', async () => {
+    const sut = new AuthenticationMiddleware()
+
+    const httpResponse = await sut.handle({ authorization: null as any })
+
+    expect(httpResponse).toEqual({
+      statusCode: 403,
+      data: new ForbiddenError()
+    })
+  })
 })
