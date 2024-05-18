e0839cdb2aafbd8045a1600edbe290e189144903
Raymw1 <rayan.rmw@gmail.com>
2024-05-18 15:00:29 -0300

    feat: ensure Authorize returns the user id of valid accessToken

--- src/domain/use-cases/authorize.ts
+++ src/domain/use-cases/authorize.ts
@@ -2,8 +2,10 @@ import { TokenValidator } from '@/domain/contracts/crypto'
 
 type Setup = (crypto: Readonly<TokenValidator>) => Authorize
 type Input = { token: string }
-export type Authorize = (params: Input) => Promise<void>
+type Output = { userId: string }
+export type Authorize = (params: Input) => Promise<Output>
 
 export const setupAuthorize: Setup = (crypto) => async params => {
-  await crypto.validateToken(params)
+  const userId = await crypto.validateToken(params)
+  return { userId }
 }
