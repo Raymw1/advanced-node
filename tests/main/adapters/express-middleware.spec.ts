4b9d2f15d61fa8efcc87768a5c9364f8b900ef72
Raymw1 <rayan.rmw@gmail.com>
2024-05-19 12:44:20 -0300

    test: ensure ExpressMiddleware calls handle with empty request

--- tests/main/adapters/express-middleware.spec.ts
+++ tests/main/adapters/express-middleware.spec.ts
@@ -16,4 +16,17 @@ describe('ExpressMiddleware', () => {
     expect(middleware.handle).toHaveBeenCalledTimes(1)
     expect(middleware.handle).toHaveBeenCalledWith({ any: 'any' })
   })
+
+  it('should call handle with empty request', async () => {
+    const httpRequest = getMockReq({})
+    const httpResponse = getMockRes().res
+    const next = getMockRes().next
+    const middleware = mock<Middleware>()
+    const sut = adaptExpressMiddleware(middleware)
+
+    await sut(httpRequest, httpResponse, next)
+
+    expect(middleware.handle).toHaveBeenCalledTimes(1)
+    expect(middleware.handle).toHaveBeenCalledWith({})
+  })
 })
