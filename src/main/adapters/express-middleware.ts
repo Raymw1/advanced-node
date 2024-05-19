431cd87025564319cea84695941c46b263b18488
Raymw1 <rayan.rmw@gmail.com>
2024-05-19 13:12:30 -0300

    feat: ensure ExpressMiddleware adds valid data to httpRequest.locals

--- src/main/adapters/express-middleware.ts
+++ src/main/adapters/express-middleware.ts
@@ -6,7 +6,8 @@ type Adapter = (middleware: Middleware) => RequestHandler
 export const adaptExpressMiddleware: Adapter = (middleware) => async (httpRequest, httpResponse, next) => {
   const { statusCode, data } = await middleware.handle({ ...httpRequest.headers })
   if (statusCode === 200) {
-    httpRequest.locals = { ...httpRequest.locals, ...data }
+    const localEntries = Object.entries(data).filter(([_key, value]) => value)
+    httpRequest.locals = { ...httpRequest.locals, ...Object.fromEntries(localEntries) }
     next()
   } else {
     httpResponse.status(statusCode).send({ error: data.message })
