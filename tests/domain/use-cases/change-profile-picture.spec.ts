e81f53a637cea35cd90d5bd4e75d4b25ec804413
Raymw1 <rayan.rmw@gmail.com>
2024-05-21 20:47:08 -0300

    test: ensure ChangeProfilePicture calls UUIDGenerator with correct input

--- tests/domain/use-cases/change-profile-picture.spec.ts
+++ tests/domain/use-cases/change-profile-picture.spec.ts
@@ -17,4 +17,16 @@ describe('ChangeProfilePicture', () => {
     expect(fileStorage.upload).toHaveBeenCalledTimes(1)
     expect(fileStorage.upload).toHaveBeenCalledWith({ file, key: uuid })
   })
+
+  it('should call UUIDGenerator with correct input', async () => {
+    const file = Buffer.from('any_buffer')
+    const fileStorage = mock<UploadFile>()
+    const crypto = mock<UUIDGenerator>()
+    const sut = setupChangeProfilePicture(fileStorage, crypto)
+
+    await sut({ userId: 'any_id', file })
+
+    expect(crypto.uuid).toHaveBeenCalledTimes(1)
+    expect(crypto.uuid).toHaveBeenCalledWith({ key: 'any_id' })
+  })
 })
