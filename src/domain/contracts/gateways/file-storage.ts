bd3cfd5278872e44a46502bbfdb99fd6e83450b7
Raymw1 <rayan.rmw@gmail.com>
2024-05-22 21:07:38 -0300

    feat: ensure ChangeProfilePicture calls DeleteFile if file exists and SaveUserPicture throws

--- src/domain/contracts/gateways/file-storage.ts
+++ src/domain/contracts/gateways/file-storage.ts
@@ -6,3 +6,11 @@ export namespace UploadFile {
   export type Input = { file: Buffer, key: string }
   export type Output = { fileUrl: string }
 }
+
+export interface DeleteFile {
+  delete: (params: DeleteFile.Input) => Promise<void>
+}
+
+export namespace DeleteFile {
+  export type Input = { key: string }
+}
--- src/domain/use-cases/change-profile-picture.ts
+++ src/domain/use-cases/change-profile-picture.ts
@@ -1,9 +1,9 @@
-import { UUIDGenerator, UploadFile } from '@/domain/contracts/gateways'
+import { DeleteFile, UUIDGenerator, UploadFile } from '@/domain/contracts/gateways'
 import { LoadUserProfileRepository, SaveUserPictureRepository } from '@/domain/contracts/repos'
 import { UserProfile } from '@/domain/entities'
 
 type Setup = (
-  fileStorage: UploadFile,
+  fileStorage: UploadFile & DeleteFile,
   crypto: UUIDGenerator,
   userProfileRepository: SaveUserPictureRepository & LoadUserProfileRepository
 ) => ChangeProfilePicture
@@ -13,14 +13,18 @@ export type ChangeProfilePicture = (params: Input) => Promise<Output>
 
 export const setupChangeProfilePicture: Setup = (fileStorage, crypto, userProfileRepository) => async ({ userId, file }) => {
   const data: { pictureUrl: string | undefined, name: string | undefined } = { pictureUrl: undefined, name: undefined }
+  const fileKey = crypto.uuid({ key: userId })
   if (file !== undefined) {
-    const fileKey = crypto.uuid({ key: userId })
     data.pictureUrl = (await fileStorage.upload({ file, key: fileKey })).fileUrl
   } else {
     data.name = (await userProfileRepository.load({ id: userId })).name
   }
   const userProfile = new UserProfile(userId)
   userProfile.setPicture(data)
-  await userProfileRepository.savePicture(userProfile)
+  try {
+    await userProfileRepository.savePicture(userProfile)
+  } catch (error) {
+    await fileStorage.delete({ key: fileKey })
+  }
   return userProfile
 }
