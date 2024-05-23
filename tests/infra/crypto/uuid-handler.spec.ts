032525784151a580e6057ed67567a9590b077bce
Raymw1 <rayan.rmw@gmail.com>
2024-05-23 20:07:42 -0300

    test: ensure UUIDHandler returns correct uuid

--- tests/infra/crypto/uuid-handler.spec.ts
+++ tests/infra/crypto/uuid-handler.spec.ts
@@ -11,4 +11,13 @@ describe('UUIDHandler', () => {
 
     expect(v4).toHaveBeenCalledTimes(1)
   })
+
+  it('should return correct uuid', () => {
+    jest.mocked(v4).mockReturnValueOnce('any_v4_uuid')
+    const sut = new UUIDHandler()
+
+    const uuid = sut.uuid({ key: 'any_key' })
+
+    expect(uuid).toBe('any_key_any_v4_uuid')
+  })
 })
