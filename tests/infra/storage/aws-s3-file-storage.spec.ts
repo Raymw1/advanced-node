e24ecaa22ed969c05fe4a3048cf1646b9ee75c82
Raymw1 <rayan.rmw@gmail.com>
2024-05-23 20:52:59 -0300

    refactor: isolate AwsS3FileStorage sut creation

--- tests/infra/storage/aws-s3-file-storage.spec.ts
+++ tests/infra/storage/aws-s3-file-storage.spec.ts
@@ -5,11 +5,20 @@ import { config } from 'aws-sdk'
 jest.mock('aws-sdk')
 
 describe('AwsS3FileStorage', () => {
-  it('should config aws credentials on creation', () => {
-    const accessKeyId = 'any_access_key_id'
-    const secretAccessKey = 'any_secret_access_key'
-    const sut = new AwsS3FileStorage(accessKeyId, secretAccessKey)
+  let accessKeyId: string
+  let secretAccessKey: string
+  let sut: AwsS3FileStorage
 
+  beforeAll(() => {
+    accessKeyId = 'any_access_key_id'
+    secretAccessKey = 'any_secret_access_key'
+  })
+
+  beforeEach(() => {
+    sut = new AwsS3FileStorage(accessKeyId, secretAccessKey)
+  })
+
+  it('should config aws credentials on creation', () => {
     expect(sut).toBeDefined()
     expect(config.update).toHaveBeenCalledTimes(1)
     expect(config.update).toHaveBeenCalledWith({ credentials: { accessKeyId, secretAccessKey } })
