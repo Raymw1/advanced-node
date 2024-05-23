4c6acb632af6e4365f095d702c6aecf902b78292
Raymw1 <rayan.rmw@gmail.com>
2024-05-23 20:08:38 -0300

    refactor: isolate UUIDHandler sut creation

--- tests/infra/crypto/uuid-handler.spec.ts
+++ tests/infra/crypto/uuid-handler.spec.ts
@@ -4,18 +4,23 @@ import { UUIDHandler } from '@/infra/crypto'
 jest.mock('uuid')
 
 describe('UUIDHandler', () => {
-  it('should call uuid.v4', () => {
-    const sut = new UUIDHandler()
+  let sut: UUIDHandler
 
+  beforeAll(() => {
+    jest.mocked(v4).mockReturnValue('any_v4_uuid')
+  })
+
+  beforeEach(() => {
+    sut = new UUIDHandler()
+  })
+
+  it('should call uuid.v4', () => {
     sut.uuid({ key: 'any_key' })
 
     expect(v4).toHaveBeenCalledTimes(1)
   })
 
   it('should return correct uuid', () => {
-    jest.mocked(v4).mockReturnValueOnce('any_v4_uuid')
-    const sut = new UUIDHandler()
-
     const uuid = sut.uuid({ key: 'any_key' })
 
     expect(uuid).toBe('any_key_any_v4_uuid')
