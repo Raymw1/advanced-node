852c67bbdd7b5eddeaea410c8e523be5241ac973
Raymw1 <rayan.rmw@gmail.com>
2024-05-23 20:00:02 -0300

    test: ensure UUIDHandler calls uuid.v4

---
+++ tests/infra/crypto/uuid-handler.spec.ts
@@ -0,0 +1,14 @@
+import { v4 } from 'uuid'
+import { UUIDHandler } from '@/infra/crypto'
+
+jest.mock('uuid')
+
+describe('UUIDHandler', () => {
+  it('should call uuid.v4', () => {
+    const sut = new UUIDHandler()
+
+    sut.uuid({ key: 'any_key' })
+
+    expect(v4).toHaveBeenCalledTimes(1)
+  })
+})
