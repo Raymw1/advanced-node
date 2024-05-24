a037b6bb90972c9f1fd6e1f6634c71fc1da77ec9
Raymw1 <rayan.rmw@gmail.com>
2024-05-24 20:36:18 -0300

    feat: ensure PgUserProfileRepository updates user profile

---
+++ src/infra/postgres/repos/user-profile.ts
@@ -0,0 +1,11 @@
+import { SaveUserPictureRepository } from '@/domain/contracts/repos'
+import { PgUser } from '@/infra/postgres/entities'
+
+import { getRepository } from 'typeorm'
+
+export class PgUserProfileRepository implements SaveUserPictureRepository {
+  async savePicture ({ id, pictureUrl, initials }: SaveUserPictureRepository.Input): Promise<void> {
+    const pgUserRepo = getRepository(PgUser)
+    await pgUserRepo.update({ id: parseInt(id) }, { pictureUrl, initials })
+  }
+}
