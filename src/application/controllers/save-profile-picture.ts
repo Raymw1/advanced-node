6a6dba55e5580261e9fefcc15e93af26784df5e0
Raymw1 <rayan.rmw@gmail.com>
2024-05-25 16:49:10 -0300

    feat: ensure SaveProfilePictureController returns 400 if file size is bigger than 5MB

--- src/application/controllers/save-profile-picture.ts
+++ src/application/controllers/save-profile-picture.ts
@@ -1,6 +1,6 @@
 import { Controller } from '@/application/controllers'
 import { HttpResponse, badRequest } from '@/application/helpers'
-import { InvalidMimeTypeError, RequiredFieldError } from '@/application/errors'
+import { InvalidMimeTypeError, MaxFileSizeError, RequiredFieldError } from '@/application/errors'
 
 type HttpRequest = { file: { buffer: Buffer, mimeType: string } }
 
@@ -9,6 +9,7 @@ export class SaveProfilePictureController extends Controller {
     if (file === undefined || file === null) return badRequest(new RequiredFieldError('file'))
     if (file.buffer.length === 0) return badRequest(new RequiredFieldError('file'))
     if (!['image/png', 'image/jpg', 'image/jpeg'].includes(file.mimeType)) return badRequest(new InvalidMimeTypeError(['png', 'jpeg']))
+    if (file.buffer.byteLength > 5 * 1024 * 1024) return badRequest(new MaxFileSizeError(5))
     throw new Error('Method not implemented.')
   }
 }
