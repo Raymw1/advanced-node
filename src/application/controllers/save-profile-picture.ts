f0ee23e46c7801651f4f41ae603573b85869ecad
Raymw1 <rayan.rmw@gmail.com>
2024-05-25 15:59:09 -0300

    feat: ensure SaveProfilePictureController returns 400 if file is not provided

---
+++ src/application/controllers/save-profile-picture.ts
@@ -0,0 +1,11 @@
+import { Controller } from '@/application/controllers'
+import { HttpResponse, badRequest } from '@/application/helpers'
+import { RequiredFieldError } from '@/application/errors'
+
+type HttpRequest = { file: { buffer: Buffer, mimeType: string } }
+
+export class SaveProfilePictureController extends Controller {
+  async perform (httpRequest: HttpRequest): Promise<HttpResponse> {
+    return badRequest(new RequiredFieldError('file'))
+  }
+}
