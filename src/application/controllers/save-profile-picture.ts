f97a57101ff6439f3e94327593c299c897f83456
Raymw1 <rayan.rmw@gmail.com>
2024-05-25 17:01:54 -0300

    feat: ensure SaveProfilePictureController returns 404 if ChangeProfilePicture throws NotFound

--- src/application/controllers/save-profile-picture.ts
+++ src/application/controllers/save-profile-picture.ts
@@ -1,7 +1,8 @@
 import { Controller } from '@/application/controllers'
-import { HttpResponse, badRequest } from '@/application/helpers'
+import { HttpResponse, badRequest, notFound } from '@/application/helpers'
 import { InvalidMimeTypeError, MaxFileSizeError, RequiredFieldError } from '@/application/errors'
 import { ChangeProfilePicture } from '@/domain/use-cases'
+import { UserProfileNotFoundError } from '@/domain/errors'
 
 type HttpRequest = { userId: string, file: { buffer: Buffer, mimeType: string } }
 
@@ -15,7 +16,12 @@ export class SaveProfilePictureController extends Controller {
     if (file.buffer.length === 0) return badRequest(new RequiredFieldError('file'))
     if (!['image/png', 'image/jpg', 'image/jpeg'].includes(file.mimeType)) return badRequest(new InvalidMimeTypeError(['png', 'jpeg']))
     if (file.buffer.byteLength > 5 * 1024 * 1024) return badRequest(new MaxFileSizeError(5))
-    await this.changeProfilePicture({ userId, file: file.buffer })
-    throw new Error('Method not implemented.')
+    try {
+      await this.changeProfilePicture({ userId, file: file.buffer })
+      throw new Error('Method not implemented.')
+    } catch (error) {
+      if (error instanceof UserProfileNotFoundError) return notFound(error)
+      throw error
+    }
   }
 }
