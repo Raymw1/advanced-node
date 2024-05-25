434ecad46d99667adbe71a90d3cf712649759b87
Raymw1 <rayan.rmw@gmail.com>
2024-05-25 17:26:06 -0300

    test: add Required validator

---
+++ tests/application/validation/required.spec.ts
@@ -0,0 +1,28 @@
+import { RequiredFieldError } from '@/application/errors'
+import { Required } from '@/application/validation'
+
+describe('Required', () => {
+  it('should return RequiredFieldError if value is null', () => {
+    const sut = new Required(null as any, 'any_field')
+
+    const error = sut.validate()
+
+    expect(error).toEqual(new RequiredFieldError('any_field'))
+  })
+
+  it('should return RequiredFieldError if value is undefined', () => {
+    const sut = new Required(undefined as any, 'any_field')
+
+    const error = sut.validate()
+
+    expect(error).toEqual(new RequiredFieldError('any_field'))
+  })
+
+  it('should return undefined if value is not empty', () => {
+    const sut = new Required('any_value', 'any_field')
+
+    const error = sut.validate()
+
+    expect(error).toBeUndefined()
+  })
+})
