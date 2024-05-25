2630b934b24d9d8816385a4cec2529dbb6245a92
Raymw1 <rayan.rmw@gmail.com>
2024-05-25 16:15:32 -0300

    feat: ensure SaveProfilePictureController returns 400 if file type is invalid

--- src/application/controllers/save-profile-picture.ts
+++ src/application/controllers/save-profile-picture.ts
@@ -1,11 +1,13 @@
 import { Controller } from '@/application/controllers'
 import { HttpResponse, badRequest } from '@/application/helpers'
-import { RequiredFieldError } from '@/application/errors'
+import { InvalidMimeTypeError, RequiredFieldError } from '@/application/errors'
 
-type HttpRequest = { file: { buffer: Buffer } }
+type HttpRequest = { file: { buffer: Buffer, mimeType: string } }
 
 export class SaveProfilePictureController extends Controller {
-  async perform (httpRequest: HttpRequest): Promise<HttpResponse> {
-    return badRequest(new RequiredFieldError('file'))
+  async perform ({ file }: HttpRequest): Promise<HttpResponse> {
+    if (file === undefined || file === null) return badRequest(new RequiredFieldError('file'))
+    if (file.buffer.length === 0) return badRequest(new RequiredFieldError('file'))
+    return badRequest(new InvalidMimeTypeError(['png', 'jpeg']))
   }
 }
