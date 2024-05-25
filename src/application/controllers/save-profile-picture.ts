682684a840ef7c2145cb8076fe0814480eba5599
Raymw1 <rayan.rmw@gmail.com>
2024-05-25 16:20:31 -0300

    feat: ensure SaveProfilePictureController accepts valid mime types

--- src/application/controllers/save-profile-picture.ts
+++ src/application/controllers/save-profile-picture.ts
@@ -8,6 +8,7 @@ export class SaveProfilePictureController extends Controller {
   async perform ({ file }: HttpRequest): Promise<HttpResponse> {
     if (file === undefined || file === null) return badRequest(new RequiredFieldError('file'))
     if (file.buffer.length === 0) return badRequest(new RequiredFieldError('file'))
-    return badRequest(new InvalidMimeTypeError(['png', 'jpeg']))
+    if (!['image/png', 'image/jpg', 'image/jpeg'].includes(file.mimeType)) return badRequest(new InvalidMimeTypeError(['png', 'jpeg']))
+    throw new Error('Method not implemented.')
   }
 }
