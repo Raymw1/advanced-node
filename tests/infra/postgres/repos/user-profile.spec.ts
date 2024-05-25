4228ab3cf6551c35c5f018e11b957dd53bec3359
Raymw1 <rayan.rmw@gmail.com>
2024-05-24 21:03:45 -0300

    test: ensure LoadUserProfileRepository returns undefined if profile does not exist

--- tests/infra/postgres/repos/user-profile.spec.ts
+++ tests/infra/postgres/repos/user-profile.spec.ts
@@ -40,4 +40,20 @@ describe('PgUserProfileRepository', () => {
       })
     })
   })
+
+  describe('load', () => {
+    it('should load user profile', async () => {
+      const { id } = await pgUserRepo.save({ email: 'any_email', name: 'any_name' })
+
+      const userProfile = await sut.load({ id: id.toString() })
+
+      expect(userProfile?.name).toBe('any_name')
+    })
+
+    it('should return undefined if profile does not exist', async () => {
+      const userProfile = await sut.load({ id: '1' })
+
+      expect(userProfile).toBeUndefined()
+    })
+  })
 })
