78b8df01d113240d63599105b30e574894c3a077
Raymw1 <rayan.rmw@gmail.com>
2024-05-25 16:57:54 -0300

    feat: ensure SaveProfilePictureController calls ChangeProfilePicture with correct input

--- src/application/controllers/save-profile-picture.ts
+++ src/application/controllers/save-profile-picture.ts
@@ -1,15 +1,21 @@
 import { Controller } from '@/application/controllers'
 import { HttpResponse, badRequest } from '@/application/helpers'
 import { InvalidMimeTypeError, MaxFileSizeError, RequiredFieldError } from '@/application/errors'
+import { ChangeProfilePicture } from '@/domain/use-cases'
 
-type HttpRequest = { file: { buffer: Buffer, mimeType: string } }
+type HttpRequest = { userId: string, file: { buffer: Buffer, mimeType: string } }
 
 export class SaveProfilePictureController extends Controller {
-  async perform ({ file }: HttpRequest): Promise<HttpResponse> {
+  constructor (private readonly changeProfilePicture: ChangeProfilePicture) {
+    super()
+  }
+
+  async perform ({ userId, file }: HttpRequest): Promise<HttpResponse> {
     if (file === undefined || file === null) return badRequest(new RequiredFieldError('file'))
     if (file.buffer.length === 0) return badRequest(new RequiredFieldError('file'))
     if (!['image/png', 'image/jpg', 'image/jpeg'].includes(file.mimeType)) return badRequest(new InvalidMimeTypeError(['png', 'jpeg']))
     if (file.buffer.byteLength > 5 * 1024 * 1024) return badRequest(new MaxFileSizeError(5))
+    await this.changeProfilePicture({ userId, file: file.buffer })
     throw new Error('Method not implemented.')
   }
 }
