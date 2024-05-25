96b8ba82b9d6505c71d63718ef5932bec93abed6
Raymw1 <rayan.rmw@gmail.com>
2024-05-25 15:29:05 -0300

    test: ensure DeleteProfilePictureController calls ChangeProfilePicture with correct input

--- tests/application/controllers/delete-profile-picture.spec.ts
+++ tests/application/controllers/delete-profile-picture.spec.ts
@@ -1,13 +1,25 @@
 import { Controller, DeleteProfilePictureController } from '@/application/controllers'
 
 describe('DeleteProfilePictureController', () => {
+  let changeProfilePicture: jest.Mock
   let sut: DeleteProfilePictureController
 
+  beforeAll(() => {
+    changeProfilePicture = jest.fn()
+  })
+
   beforeEach(() => {
-    sut = new DeleteProfilePictureController()
+    sut = new DeleteProfilePictureController(changeProfilePicture)
   })
 
   it('should extend Controller', () => {
     expect(sut).toBeInstanceOf(Controller)
+  })
+
+  it('should call ChangeProfilePicture with correct input', async () => {
+    await sut.handle({ userId: 'any_user_id' })
+
+    expect(changeProfilePicture).toHaveBeenCalledTimes(1)
+    expect(changeProfilePicture).toHaveBeenCalledWith({ userId: 'any_user_id' })
   })
 })
