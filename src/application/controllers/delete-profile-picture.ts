ae32226f8fb51b9c3b434e7debf2dcbc64ace1d9
Raymw1 <rayan.rmw@gmail.com>
2024-05-25 15:16:16 -0300

    feat: ensure DeleteProfilePictureController extends Controller

---
+++ src/application/controllers/delete-profile-picture.ts
@@ -0,0 +1,8 @@
+import { Controller } from '@/application/controllers'
+import { HttpResponse } from '@/application/helpers'
+
+export class DeleteProfilePictureController extends Controller {
+  async perform (httpRequest: any): Promise<HttpResponse> {
+    throw new Error('Method not implemented.')
+  }
+}
