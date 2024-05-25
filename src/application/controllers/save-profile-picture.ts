a2fbd619ea8c87c87caa66608f67a24ea74f16eb
Raymw1 <rayan.rmw@gmail.com>
2024-05-25 16:03:49 -0300

    test: ensure SaveProfilePictureController returns 400 if file is empty

--- src/application/controllers/save-profile-picture.ts
+++ src/application/controllers/save-profile-picture.ts
@@ -2,7 +2,7 @@ import { Controller } from '@/application/controllers'
 import { HttpResponse, badRequest } from '@/application/helpers'
 import { RequiredFieldError } from '@/application/errors'
 
-type HttpRequest = { file: any }
+type HttpRequest = { file: { buffer: Buffer } }
 
 export class SaveProfilePictureController extends Controller {
   async perform (httpRequest: HttpRequest): Promise<HttpResponse> {
