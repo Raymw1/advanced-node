1217b0e2965548d065e1f35356cd9ee1f1f64a4f
Raymw1 <rayan.rmw@gmail.com>
2024-05-26 11:48:52 -0300

    test: ensure AllowedMimeTypes returns InvalidMimeTypeError if value is invalid

---
+++ tests/application/validation/allowed-mime-types.spec.ts
@@ -0,0 +1,12 @@
+import { InvalidMimeTypeError } from '@/application/errors'
+import { AllowedMimeTypes } from '@/application/validation'
+
+describe('AllowedMimeTypes', () => {
+  it('should return InvalidMimeTypeError if value is invalid', () => {
+    const sut = new AllowedMimeTypes(['png'], 'image/jpg')
+
+    const error = sut.validate()
+
+    expect(error).toEqual(new InvalidMimeTypeError(['png']))
+  })
+})
