5b1ee418da0888e6103ea4669af42769d17aec51
Raymw1 <rayan.rmw@gmail.com>
2024-05-26 12:17:29 -0300

    test: ensure MaxFileSize returns undefined if value is valid

--- tests/application/validation/max-file-size.spec.ts
+++ tests/application/validation/max-file-size.spec.ts
@@ -10,4 +10,20 @@ describe('MaxFileSize', () => {
 
     expect(error).toEqual(new MaxFileSizeError(2))
   })
+
+  it('should return undefined if value is valid', () => {
+    const sut = new MaxFileSize(2, Buffer.from('any_buffer'))
+
+    const error = sut.validate()
+
+    expect(error).toBeUndefined()
+  })
+
+  it('should return undefined if value is valid', () => {
+    const sut = new MaxFileSize(2, Buffer.from(new ArrayBuffer(2 * 1024 * 1024)))
+
+    const error = sut.validate()
+
+    expect(error).toBeUndefined()
+  })
 })
