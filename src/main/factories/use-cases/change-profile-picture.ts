4d3f5ddf414b1fc08e672e3e923cdfd8a6dd68e3
Raymw1 <rayan.rmw@gmail.com>
2024-06-01 17:15:26 -0300

    refactor: organize code

--- src/main/factories/use-cases/change-profile-picture.ts
+++ src/main/factories/use-cases/change-profile-picture.ts
@@ -1,6 +1,6 @@
 import { ChangeProfilePicture, setupChangeProfilePicture } from '@/domain/use-cases'
 import { makeAwsS3FileStorage, makeUUIDHandler } from '@/main/factories/gateways'
-import { makePgUserProfileRepository } from '@/main/factories/repos'
+import { makePgUserProfileRepository } from '@/main/factories/postgres/repos'
 
 export const makeChangeProfilePicture = (): ChangeProfilePicture => {
   return setupChangeProfilePicture(makeAwsS3FileStorage(), makeUUIDHandler(), makePgUserProfileRepository())
--- src/main/factories/use-cases/facebook-authentication.ts
+++ src/main/factories/use-cases/facebook-authentication.ts
@@ -1,6 +1,6 @@
 import { FacebookAuthentication, setupFacebookAuthentication } from '@/domain/use-cases'
 import { makeFacebookApi, makeJwtTokenHandler } from '@/main/factories/gateways'
-import { makePgUserAccountRepository } from '@/main/factories/repos'
+import { makePgUserAccountRepository } from '@/main/factories/postgres/repos'
 
 export const makeFacebookAuthentication = (): FacebookAuthentication => {
   return setupFacebookAuthentication(makeFacebookApi(), makePgUserAccountRepository(), makeJwtTokenHandler())
