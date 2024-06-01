e2b34e7ee7a4a3d2b84da40674a016f9c3e5541f
Raymw1 <rayan.rmw@gmail.com>
2024-05-31 21:42:47 -0300

    refactor: isolate UniqueId sut creation

--- tests/infra/crypto/unique-id.spec.ts
+++ tests/infra/crypto/unique-id.spec.ts
@@ -3,23 +3,29 @@ import { UniqueId } from '@/infra/crypto'
 import mockdate from 'mockdate'
 
 describe('UniqueId', () => {
-  it('should create a valid unique id', () => {
-    mockdate.set(new Date(2021, 9, 9, 10, 10, 10))
-    const sut = new UniqueId()
+  let sut: UniqueId
 
-    const uuid = sut.uuid({ key: 'any_key' })
+  beforeEach(() => {
+    sut = new UniqueId()
+  })
 
-    expect(uuid).toBe('any_key_20210909101010')
+  afterEach(() => {
     mockdate.reset()
   })
 
   it('should create a valid unique id', () => {
+    mockdate.set(new Date(2021, 9, 9, 10, 10, 10))
+
+    const uuid = sut.uuid({ key: 'any_key' })
+
+    expect(uuid).toBe('any_key_20210909101010')
+  })
+
+  it('should create a valid unique id', () => {
     mockdate.set(new Date(1989, 1, 2, 12, 0, 59))
-    const sut = new UniqueId()
 
     const uuid = sut.uuid({ key: 'any_key' })
 
     expect(uuid).toBe('any_key_19890102120059')
-    mockdate.reset()
   })
 })
