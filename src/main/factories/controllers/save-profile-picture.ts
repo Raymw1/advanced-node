073cd3ea08127f86aa5e68171e21fa85a3e9bc27
Raymw1 <rayan.rmw@gmail.com>
2024-06-01 17:31:33 -0300

    feat: add PgTransactionController factory

--- src/main/factories/controllers/save-profile-picture.ts
+++ src/main/factories/controllers/save-profile-picture.ts
@@ -1,6 +1,8 @@
-import { SaveProfilePictureController } from '@/application/controllers'
+import { Controller, SaveProfilePictureController } from '@/application/controllers'
+import { makePgTransactionController } from '@/main/factories/decorators'
 import { makeChangeProfilePicture } from '@/main/factories/use-cases'
 
-export const makeSaveProfilePictureController = (): SaveProfilePictureController => {
-  return new SaveProfilePictureController(makeChangeProfilePicture())
+export const makeSaveProfilePictureController = (): Controller => {
+  const controller = new SaveProfilePictureController(makeChangeProfilePicture())
+  return makePgTransactionController(controller)
 }
