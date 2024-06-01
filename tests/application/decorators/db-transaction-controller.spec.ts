d40c164627830bd1450e19cc0e23483ed3c6cb84
Raymw1 <rayan.rmw@gmail.com>
2024-06-01 16:42:53 -0300

    test: ensure DbTransactionController executes decoratee

--- tests/application/decorators/db-transaction-controller.spec.ts
+++ tests/application/decorators/db-transaction-controller.spec.ts
@@ -1,18 +1,21 @@
 import { DbTransaction } from '@/application/contracts'
+import { Controller } from '@/application/controllers'
 import { DbTransactionController } from '@/application/decorators'
 
 import { MockProxy, mock } from 'jest-mock-extended'
 
 describe('DbTransactionController', () => {
   let db: MockProxy<DbTransaction>
+  let decoratee: MockProxy<Controller>
   let sut: DbTransactionController
 
   beforeAll(() => {
     db = mock<DbTransaction>()
+    decoratee = mock<Controller>()
   })
 
   beforeEach(() => {
-    sut = new DbTransactionController(db)
+    sut = new DbTransactionController(decoratee, db)
   })
 
   it('should open transaction', async () => {
@@ -20,5 +23,12 @@ describe('DbTransactionController', () => {
 
     expect(db.openTransaction).toHaveBeenCalledTimes(1)
     expect(db.openTransaction).toHaveBeenCalledWith()
+  })
+
+  it('should execute decoratee', async () => {
+    await sut.perform({ any: 'any' })
+
+    expect(decoratee.handle).toHaveBeenCalledTimes(1)
+    expect(decoratee.handle).toHaveBeenCalledWith({ any: 'any' })
   })
 })
