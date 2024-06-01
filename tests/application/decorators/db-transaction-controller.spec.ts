55ae743e2cacac957a840ae455a252cdbc2f2846
Raymw1 <rayan.rmw@gmail.com>
2024-06-01 16:36:33 -0300

    refactor: isolate DbTransactionController sut creation

--- tests/application/decorators/db-transaction-controller.spec.ts
+++ tests/application/decorators/db-transaction-controller.spec.ts
@@ -1,12 +1,21 @@
 import { DbTransaction } from '@/application/contracts'
 import { DbTransactionController } from '@/application/decorators'
-import { mock } from 'jest-mock-extended'
+
+import { MockProxy, mock } from 'jest-mock-extended'
 
 describe('DbTransactionController', () => {
-  it('should open transaction', async () => {
-    const db = mock<DbTransaction>()
-    const sut = new DbTransactionController(db)
+  let db: MockProxy<DbTransaction>
+  let sut: DbTransactionController
 
+  beforeAll(() => {
+    db = mock<DbTransaction>()
+  })
+
+  beforeEach(() => {
+    sut = new DbTransactionController(db)
+  })
+
+  it('should open transaction', async () => {
     await sut.perform({ any: 'any' })
 
     expect(db.openTransaction).toHaveBeenCalledTimes(1)
